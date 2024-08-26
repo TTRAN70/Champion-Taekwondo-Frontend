471681f73515dcf2c4994b71b9969cfd9d9332d7
TTRAN70 <tonytran124589@gmail.com>
2024-08-25 21:24:58 -0400

    removing extras

--- src/app/fonts.ts
+++
@@ -1,4 +0,0 @@
-import { Josefin_Sans, Staatliches, Inter } from "next/font/google";
-export const staat = Staatliches({ subsets: ["latin"], weight: "400" });
-export const josefin = Josefin_Sans({ subsets: ["latin"] });
-export const inter = Inter({ subsets: ["latin"] });
