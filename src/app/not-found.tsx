554264d1ca91a9f775215ae70adf0fe776641514
TTRAN70 <tonytran124589@gmail.com>
2024-09-18 02:13:52 -0400

    Added missing page, now we ready for deployment

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <h1
+      className={`min-h-screen flex justify-center mt-20 text-3xl font-extrabold`}
+    >
+      Page not found!
+    </h1>
+  );
+}
